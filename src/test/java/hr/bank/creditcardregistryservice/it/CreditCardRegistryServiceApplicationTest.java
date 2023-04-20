342938f5dbad42b7c20339a00c61647d32bd9504
Teo <tmanojlovic@riteh.hr>
2023-04-20 10:37:32 +0200

    minor improvements in tests

--- src/test/java/hr/bank/creditcardregistryservice/it/CreditCardRegistryServiceApplicationTest.java
+++ src/test/java/hr/bank/creditcardregistryservice/it/CreditCardRegistryServiceApplicationTest.java
@@ -7,7 +7,6 @@ import hr.bank.creditcardregistryservice.utils.CreditCardInfoFileUtils;
 import hr.bank.creditcardregistryservice.utils.CustomUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,7 +15,6 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
